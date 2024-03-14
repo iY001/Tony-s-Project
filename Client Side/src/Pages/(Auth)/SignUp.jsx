2cdd94cb54edbd1afb37dd6ce6b2bda9eaee14d9
Mustafa Gamal <mustafagamal51112>
2024-03-14 22:46:22 +0200

    login & signUp

---
+++ Client Side/src/Pages/(Auth)/SignUp.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
+import Swal from 'sweetalert2';
+import { CiLock, CiLogin, CiMail } from "react-icons/ci";
+import './login.css'
+import { Link } from 'react-router-dom';
+
+function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const signUpFormHTML = ReactDOMServer.renderToString(<SignUpForm setName={setName} setEmail={setEmail} setPassword={setPassword} />);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 flex items-center gap-2 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={() => {
+          Swal.fire({
+            title: 'Sign Up',
+            html: signUpFormHTML,
+            showCancelButton: false,
+            showConfirmButton: false,
+          });
+        }}>
+        Sign Up
+        <span className='font-bold text-xl'><CiLogin /></span>
+      </button>
+    </div>
+  );
+}
+
+export default SignUp;
+
+// SignUpForm component
+export function SignUpForm({ setName, setEmail, setPassword }) {
+  return (
+    <form className="form">
+      <div className="flex-column text-start">
+        <label>Name</label>
+      </div>
+      <div className="inputForm">
+        <div className='font-bold text-black text-xl'><CiLock /></div>
+        <input placeholder="Enter your Name" className="input" type="text" onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div className="flex-column text-start">
+        <label>Email</label>
+      </div>
+      <div className="inputForm">
+        <div className='font-bold text-black text-xl'><CiMail /></div>
+        <input placeholder="Enter your Email" className="input" type="email" onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className="flex-column text-start">
+        <label>Password</label>
+      </div>
+      <div className="inputForm">
+        <div className='font-bold text-black text-xl'><CiLock /></div>
+        <input placeholder="Enter your Password" className="input" type="password" onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button className="button-submit bg-blue-500">Sign Up</button>
+      <p className="p">Already have an account? <a href="/auth/login"  className="span">Sign In</a>
+      </p><p className="p line">Or With</p>
+      <div className="flex-row">
+        <button className="btn google">
+          <svg xmlSpace="preserve" style={{ enableBackground: 'new 0 0 512 512' }} viewBox="0 0 512 512" y="0px" x="0px" xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" id="Layer_1" width={20} version="1.1">
+            <path d="M113.47,309.408L95.648,375.94l-65.139,1.378C11.042,341.211,0,299.9,0,256
+            c0-42.451,10.324-82.483,28.624-117.732h0.014l57.992,10.632l25.404,57.644c-5.317,15.501-8.215,32.141-8.215,49.456
+            C103.821,274.792,107.225,292.797,113.47,309.408z" style={{ fill: '#FBBB00' }} />
+            <path d="M507.527,208.176C510.467,223.662,512,239.655,512,256c0,18.328-1.927,36.206-5.598,53.451
+            c-12.462,58.683-45.025,109.925-90.134,146.187l-0.014-0.014l-73.044-3.727l-10.338-64.535
+            c29.932-17.554,53.324-45.025,65.646-77.911h-136.89V208.176h138.887L507.527,208.176L507.527,208.176z" style={{ fill: '#518EF8' }} />
+            <path d="M416.253,455.624l0.014,0.014C372.396,490.901,316.666,512,256,512
+            c-97.491,0-182.252-54.491-225.491-134.681l82.961-67.91c21.619,57.698,77.278,98.771,142.53,98.771
+            c28.047,0,54.323-7.582,76.87-20.818L416.253,455.624z" style={{ fill: '#28B446' }} />
+            <path d="M419.404,58.936l-82.933,67.896c-23.335-14.586-50.919-23.012-80.471-23.012
+            c-66.729,0-123.429,42.957-143.965,102.724l-83.397-68.276h-0.014C71.23,56.123,157.06,0,256,0
+            C318.115,0,375.068,22.126,419.404,58.936z" style={{ fill: '#F14336' }} />
+          </svg>
+          Google
+        </button>
+      </div>
+    </form>
+  )
+}
--- Client Side/src/Routes/MasterRoutes.jsx
+++ Client Side/src/Routes/MasterRoutes.jsx
@@ -3,13 +3,15 @@ import { Route, Routes } from 'react-router-dom'
 import Blog from '../Pages/(Blog)/Blog'
 import Dashbaord from '../Pages/(Dashboard)/Dashbaord'
 import Home from '../Pages/(LandingPage)/Home'
+import Auth from '../Pages/(Auth)/Auth'
 function MasterRoutes() {
   return (
     <>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/blog/*' element={<Blog/>}/>
-      <Route path='/dashboard' element={<Dashbaord/>}/>
+      <Route path='/dashboard/*' element={<Dashbaord/>}/>
+      <Route path='/auth/:mode' element={<Auth />}/>
       <Route path='/*' element={<h1>Page Not Found</h1>}/>
     </Routes>
     </>
