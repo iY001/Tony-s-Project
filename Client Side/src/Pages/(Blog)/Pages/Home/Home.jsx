9e530035e24209bc184e20f920ed6fff8c3c987a
3mR <amrgouda127@gmail.com>
2024-03-15 14:09:24 +0200

    pc Navbar

--- Client Side/src/Pages/(Blog)/Pages/Home/Home.jsx
+++ Client Side/src/Pages/(Blog)/Pages/Home/Home.jsx
@@ -17,17 +17,17 @@ function Home() {
     setOpenMenue(!openMenue)
   }
   return (
-    <div className='lg:w-9/12  w-full mx-auto bg-[#181A2A] dark:bg-white'>
-      <div className='container bg-white   '>
+    <div className='lg:w-9/12  w-full  bg-[#181A2A] dark:bg-white'>
+      <div className='container bg-white 2xl:w-full  '>
 
 
-        <div className={`navbar   max-lg:dark:bg-[#F4F4F5] md:absolute md:mr-10 w-full flex max-sm:flex-wrap  justify-between   max-sm:gap-2 max-lg:py-4 ${!openMenue?'max-lg:shadow-md':'max-lg:shadow-none'}   lg:my-8 items-center dark:text-black `}>
+        <div className={`navbar   2xl:justify-evenly 2xl:w-full max-lg:dark:bg-[#F4F4F5] md:absolute md:mr-10 w-full flex max-sm:flex-wrap  justify-between   max-sm:gap-2 max-lg:py-4 ${!openMenue?'max-lg:shadow-md':'max-lg:shadow-none'}   lg:my-8 items-center dark:text-black `}>
           <div className="logo  font-semibold max-lg:text-4xl max-lg:items-center max-lg:mx-auto  text-xl max-lg:flex lg:text-3xl">Meta<span className='font-bold'>BLog</span> <div className='max-sm:block hidden w-20'></div>  
           <div className='md:absolute md:left-10' onClick={() => toggleMenue()}> {!openMenue ? <IoMdMenu className='text-5xl hidden max-lg:block' /> : <IoMdClose className='text-5xl  hidden max-lg:block' />} 
           </div></div>
 
-          <div className={`navLinks max-lg:absolute max-lg:shadow-md max-lg:h-[230px] md:h-[300px]  max-lg:w-full ${openMenue?'max-lg:block':'max-lg:hidden'} max-lg:-z-10 md:-top-[58px]  max-lg:-top-2 duration-200 max-lg:dark:bg-[#F4F4F5]  max-lg:text-left max-lg:translate-y-[130px]`}>
-            <ul className={`flex max-lg:text-3xl max-sm:text-xl gap-4 lg:text-xl text-[#3B3C4A] max-lg:justify-between max-lg:flex max-lg:flex-col max-lg:my-5 max-lg:mx-10 `} >{NavLinks.map((link, i) => (<li key={i} >{link.pageName}</li>))}</ul>
+          <div className={`navLinks max-lg:absolute max-lg:shadow-md max-lg:h-[230px] md:h-[300px] 2xl:h-auto max-lg:w-full ${openMenue?'max-lg:block':'max-lg:hidden'} max-lg:-z-10 md:-top-[58px] max-lg:-top-2 duration-200 max-lg:dark:bg-[#F4F4F5]  max-lg:text-left max-lg:translate-y-[130px]`}>
+            <ul className={`flex  max-lg:text-3xl max-sm:text-xl gap-4 lg:text-xl text-[#3B3C4A] max-lg:justify-between max-lg:flex max-lg:flex-col max-lg:my-5 max-lg:mx-10 `} >{NavLinks.map((link, i) => (<li key={i} >{link.pageName}</li>))}</ul>
           </div>
 
           <div className="search-mood flex max-sm:w-full justify-between gap-8 items-center ">
