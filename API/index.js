e5d0d7dc5ef721ce61cc7e49488c82564bfd24f8
Y1 <youssefabdo4005@gmail.com>
2024-03-16 14:12:06 +0200

    --cors

--- API/index.js
+++ API/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000 || process.env.port
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Tonys Project API'))
 
