586591bf8d477fe161a09431d3bb528278f2964f
Yousef Gamel <elgamelyousef6@gmail.com>
2024-03-18 15:29:50 +0200

    About

--- Client Side/src/Pages/(LandingPage)/components/About.jsx
+++ Client Side/src/Pages/(LandingPage)/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <div className='w-full py-5 h-auto'>
         <div className='w-full flex flex-col justify-center items-center '>
             <h1 className='text-7xl font-bold uppercase'>About me<span className='text-[#f57573]'>.</span></h1>
-            <p className=' w-[75%] mt-3 text-center text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid et explicabo excepturi! Quaerat illo sunt asperiores tempore perferendis provident aliquid earum velit magni veritatis, sapiente nesciunt! Dolore quasi dicta molestiae? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit.</p>
+            <p className=' w-[75%] mt-3 text-center  md:text-lg lg:text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid et explicabo excepturi! Quaerat illo sunt asperiores tempore perferendis provident aliquid earum velit magni veritatis, sapiente nesciunt! Dolore quasi dicta molestiae? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit.</p>
             <video src={video} controls className='mt-3 w-[70%] mx-auto py-10'></video>
         </div>
     </div>
