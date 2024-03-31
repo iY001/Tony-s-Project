28f46a9408fd3a1d5f4d75bd3885aa075bf1e1d8
Mustafa Gamal <mustafa.gamal.elsayed@gmail.com>
2024-03-31 22:39:30 +0200

    --navbar

--- Client Side/src/Pages/(Blog)/Pages/Home/Home.jsx
+++ Client Side/src/Pages/(Blog)/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Navbar/Navbar';
 import Hero from './Hero/Hero';
 import LatestPosts from './posts/LatestPosts';
+import MainNavbar from '../../../../components/MainNavbar';
 
 
 function Home() {
@@ -9,7 +10,8 @@ function Home() {
   return (
     <>
       
-        <Navbar />
+        {/* <Navbar /> */}
+        <MainNavbar />
         <Hero />
         <LatestPosts/>
       
